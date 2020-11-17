1ccb78cfe5e27ac899dc978f1e1cf2cf90621432
Shota <clarinetman.morning@gmail.com>
2020-11-17 11:43:05 +0900

    SQLインジェクションを修正しました

--- www/model/cart.php
+++ www/model/cart.php
@@ -21,9 +21,9 @@ function get_user_carts($db, $user_id){
     ON
       carts.item_id = items.item_id
     WHERE
-      carts.user_id = {$user_id}
+      carts.user_id = ?
   ";
-  return fetch_all_query($db, $sql);
+  return fetch_all_query($db, $sql,$user_id);
 }
 
 function get_user_cart($db, $user_id, $item_id){
@@ -45,12 +45,12 @@ function get_user_cart($db, $user_id, $item_id){
     ON
       carts.item_id = items.item_id
     WHERE
-      carts.user_id = {$user_id}
+      carts.user_id = ?
     AND
-      items.item_id = {$item_id}
+      items.item_id = ?
   ";
 
-  return fetch_query($db, $sql);
+  return fetch_query($db, $sql,$user_id,$item_id);
 
 }
 
@@ -73,7 +73,7 @@ function insert_cart($db, $user_id, $item_id, $amount = 1){
     VALUES(?,?,?)
   ";
 
-  return execute_query($db, $sql, [$item_id,$user_id,$amount]);
+  return execute_query($db, $sql, [$user_id,$item_id,$amount]);
 }
 
 function update_cart_amount($db, $cart_id, $amount){
@@ -86,7 +86,7 @@ function update_cart_amount($db, $cart_id, $amount){
       cart_id = ?
     LIMIT 1
   ";
-  return execute_query($db, $sql, [$cart_id,$amount]);
+  return execute_query($db, $sql, [$amount,$cart_id]);
 }
 
 function delete_cart($db, $cart_id){
@@ -94,11 +94,11 @@ function delete_cart($db, $cart_id){
     DELETE FROM
       carts
     WHERE
-      cart_id = {$cart_id}
+      cart_id = ?
     LIMIT 1
   ";
 
-  return execute_query($db, $sql);
+  return execute_query($db, $sql,$cart_id);
 }
 
 function purchase_carts($db, $carts){
@@ -123,10 +123,10 @@ function delete_user_carts($db, $user_id){
     DELETE FROM
       carts
     WHERE
-      user_id = {$user_id}
+      user_id = ?
   ";
 
-  execute_query($db, $sql);
+  execute_query($db, $sql,$user_id);
 }
 
 
--- www/model/item.php
+++ www/model/item.php
@@ -16,10 +16,10 @@ function get_item($db, $item_id){
     FROM
       items
     WHERE
-      item_id = {$item_id}
+      item_id = ?
   ";
 
-  return fetch_query($db, $sql);
+  return fetch_query($db, $sql,$item_id);
 }
 
 function get_items($db, $is_open = false){
@@ -99,7 +99,7 @@ function update_item_status($db, $item_id, $status){
     LIMIT 1
   ";
   
-  return execute_query($db, $sql, [$item_id,$status]);
+  return execute_query($db, $sql, [$status,$item_id]);
 }
 
 function update_item_stock($db, $item_id, $stock){
@@ -113,7 +113,7 @@ function update_item_stock($db, $item_id, $stock){
     LIMIT 1
   ";
   
-  return execute_query($db, $sql, [$item_id,$status]);
+  return execute_query($db, $sql, [$stock,$item_id]);
 }
 
 function destroy_item($db, $item_id){
@@ -136,11 +136,11 @@ function delete_item($db, $item_id){
     DELETE FROM
       items
     WHERE
-      item_id = {$item_id}
+      item_id = ?
     LIMIT 1
   ";
   
-  return execute_query($db, $sql);
+  return execute_query($db, $sql,$item_id);
 }
 
 
