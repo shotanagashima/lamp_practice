f0d1440045be759f433aadb86b0bdc2f58261e66
Shota <clarinetman.morning@gmail.com>
2020-11-21 23:28:21 +0900

    購入履歴処理を追加#12

---
+++ www/model/order_history.php
@@ -0,0 +1,17 @@
+<?php
+require_once MODEL_PATH . 'functions.php';
+require_once MODEL_PATH . 'db.php';
+
+// DB利用
+function insert_order_history($db, $user_id, $total_price) {
+    $sql = "
+      INSERT INTO
+        order_histories(
+            user_id,
+            total_price
+        )
+      VALUES(?,?);
+    ";
+    $parameters = [$user_id, $total_price];
+    return execute_query($db, $sql, $parameters);
+}
